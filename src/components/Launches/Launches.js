c819a9adbe798ea2155294aa385f83c016fce1b1
IvaschenkoAndrii <ivaschenko84@gmail.com>
2023-07-06 19:18:13 +0200

    hw2_2

--- src/components/Launches/Launches.js
+++ src/components/Launches/Launches.js
@@ -1,20 +1,23 @@
 import {useEffect, useState} from "react";
 import {Launch} from "../Launch/Launch";
 
-function Launches (){
+function Launches() {
 
     const [launches, setLaunches] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://api.spacexdata.com/v3/launches/')
             .then(value => value.json())
             .then(value => setLaunches(value))
-    },[])
+    }, [])
 
 
     return (
         <div>
-            {launches.filter(value => value.launch_year!=='2020').map((launch, id) => <Launch key={id} launch={launch}/>)}
+            {launches.filter(value => value.launch_year !== '2020')
+                .map((launch, id) =>
+                    <Launch key={id}
+                            launch={launch}/>)}
         </div>
     )
 }
