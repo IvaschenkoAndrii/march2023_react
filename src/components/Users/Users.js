d8cffeaae2fb2f786c3dca1b2db9425d2b4b028f
IvaschenkoAndrii <ivaschenko84@gmail.com>
2023-07-06 16:45:54 +0200

    hw1_3

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,22 +1,23 @@
 import {useEffect, useState} from "react";
 import {User} from "../User/User";
 
-function Users (){
+function Users() {
 
     const [users, setUsers] = useState([]);
     const [posts, setPosts] = useState([]);
 
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(value => value.json())
-            .then(value => console.log(value))
-    },[])
+    // useEffect(() => {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(value => value.json())
+    //         .then(value => console.log(value))
+    // }, [])
 
 
+    fetch('https://jsonplaceholder.typicode.com/users').then(r => r.json().then(r=> console.log(r)))
 
     return (
         <div>
-            {users?.map((user,id)=><User key={id} user={user}/>)}
+            {users?.map((user, id) => <User key={id} user={user}/>)}
         </div>
     )
 }
